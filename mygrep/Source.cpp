f4145ff9f8a5066e1294678ef27c9d82e62c69fa
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2021-02-17 02:35:50 +0200

    Changed the search function to use C strings

--- mygrep/Source.cpp
+++ mygrep/Source.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 #include <string>
+#include <string.h>
 using namespace std;
 
-void simpleSearch();
+void simpleSearch(const char*, const char*);
+void askForInputAndSearch();
 
 int main(int argc, char *argv[]){   // get arguments from binary ran from cli
 
-
     if (argc == 1) // if no arguments provided, ask the user for input
     {
-        simpleSearch();
+        askForInputAndSearch();
     }
 }
 
-void simpleSearch(){
+void askForInputAndSearch(){
     string stringToSearchFrom, stringToSearch;
-    double position;
 
     cout << "Give a string from which to search for: ";
     getline(cin, stringToSearchFrom);
     cout << "Give search string: ";
     getline(cin, stringToSearch);
 
-    position = stringToSearchFrom.find(stringToSearch); //returns index for needle in haystack if found OR return string::npos (max value)
+    simpleSearch(stringToSearchFrom.c_str(), stringToSearch.c_str());
+}
 
-    if (position != string::npos) // check for match, if found, print strings and index
+void simpleSearch(const char* stringToSearchFrom, const char* stringToSearch){
+    
+    const char* result = strstr(stringToSearchFrom, stringToSearch); // returns a pointer to the first occurrence of stringToSearch in stringToSearchFrom, or a null pointer if stringToSearch is not part of stringToSearchFrom
+
+    if (result != NULL) // check for match, if found, print strings and index
         {
+            int position = result-stringToSearchFrom; // calculate substring's index in bigstring
             cout << "\n" << '"' << stringToSearch << '"' << " found in \"" << stringToSearchFrom << '"' << " in position " << position << endl;
         }
     else // if no match, print NOT found
