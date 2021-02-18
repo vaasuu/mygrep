f539a27f6a34575aa4b15737b6dcd9d9c7bac651
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2021-02-19 01:06:05 +0200

    Added basic cli argument support

--- mygrep/Source.cpp
+++ mygrep/Source.cpp
@@ -6,8 +6,14 @@ using namespace std;
 
 void simpleSearch(const char*, const char*);
 void askForInputAndSearch();
+bool stringContainsChar(string str, string character);
 
 int main(int argc, char *argv[]){   // get arguments from binary ran from cli
+
+    bool printOccuranceCount = false;
+    bool printLineNumbers = false;
+    bool ExcludeSearchLines = false;
+    bool IgnoreCase = false;
 
     if (argc == 1) // if no arguments provided, ask the user for input
     {
@@ -22,8 +28,8 @@ int main(int argc, char *argv[]){   // get arguments from binary ran from cli
         // char* stringToSearch = "following"; // debug
         // char* filename = "../teht-anto/man_grep_plain_ASCII.txt"; // debug
 
-        cout << stringToSearch << endl;
-        cout << filename << endl;
+        // cout << stringToSearch << endl;
+        // cout << filename << endl;
 
         string line;
         ifstream myfile;
@@ -39,6 +45,65 @@ int main(int argc, char *argv[]){   // get arguments from binary ran from cli
                     cout << line << endl;
                 }
             }
+        }
+        else
+        {
+            cerr << "Error opening file " << filename << endl;
+        }
+    }
+
+    if (argc == 4)
+    {
+
+        string options_arg = argv[1];
+        char* stringToSearch = argv[2];
+        char* filename = argv[3];
+
+        string options = options_arg.substr(2,string::npos);
+
+        
+        if (stringContainsChar(options, "l"))
+        {
+            printLineNumbers = true;
+        }
+        if (stringContainsChar(options, "o"))
+        {
+            printOccuranceCount = true;
+        }        
+
+        // char* stringToSearch = "following"; // debug
+        // char* filename = "../teht-anto/man_grep_plain_ASCII.txt"; // debug
+
+        int currentLine = 0;
+        int countOccurrences = 0;
+
+        string line;
+        ifstream myfile;
+        myfile.open(filename);
+        if (myfile.is_open())
+        {
+            while (!myfile.eof())
+            {
+                getline(myfile, line);
+                currentLine++;
+                const char* result = strstr(line.c_str(), stringToSearch);
+                if (result != NULL)
+                {
+                    countOccurrences++;
+
+                    if (printLineNumbers)
+                    {
+                        cout << currentLine << ": ";
+                    }
+                    
+                    cout << line << endl;
+                }
+            }
+            if (printOccuranceCount)
+            {
+                cout << "\nOccurrences of lines containing "<< stringToSearch << "\": " << countOccurrences << endl;
+            }
+            
         }
         else
         {
@@ -71,4 +136,15 @@ void simpleSearch(const char* stringToSearchFrom, const char* stringToSearch){
         {
             cout << "\n" << '"' << stringToSearch << '"' << " NOT found in \"" << stringToSearchFrom << '"' << endl;
         } 
+}
+
+bool stringContainsChar(string str, string character){
+    if (str.find(character) != string::npos)
+        {
+            return true;
+        }
+    else
+    {
+        return false;
+    }
 }
